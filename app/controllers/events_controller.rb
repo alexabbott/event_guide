4a61e20a37951f156477b042370b9ee5ead5021b
alexabbott <alex.r.abbott@gmail.com>
2014-10-30 22:57:12 -0700

    Add event filters

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,34 @@
 class EventsController < ApplicationController
 
 def index
-	@events = Event.all.sort_by &:date
+	category_filter = params[:categoryfilter]
+	case category_filter
+	when "art_walk"
+		@events = Event.where(:category => "art walk")
+	when "community"
+		@events = Event.where(:category => "community")
+	when "festival"
+		@events = Event.where(:category => "festival")
+	when "free_booze"
+		@events = Event.where(:category => "free booze")
+	when "free_food"
+		@events = Event.where(:category => "free food")
+	when "free_music"
+		@events = Event.where(:category => "free music")	
+	when "free_party"
+		@events = Event.where(:category => "free party")
+	when "free_yoga"
+		@events = Event.where(:category => "free yoga")
+	when "street_fair"
+		@events = Event.where(:category => "street fair")
+	when "other"
+		@events = Event.where(:category => "other")
+	when "all_events"
+		@events = Event.all.sort_by &:date
+	else
+		@events = Event.all.sort_by &:date
+	end
+	# @events = Event.all.sort_by &:date
 	@fevents = Fevent.all.sort_by &:date
 end
 
