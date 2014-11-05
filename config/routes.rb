b6ca2f4c1cacf413fe10c24ba3e70aaada4359af
alexabbott <alex.r.abbott@gmail.com>
2014-11-05 10:54:43 -0800

    Add redirect to home page on undefined path

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,8 @@ Eventguide::Application.routes.draw do
  # DELETE   
  # ---  DELETE  
   delete 'fevents/:id' => 'fevents#destroy', as: :delete_fevent
+
+  get '*path' => redirect('/')
  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
